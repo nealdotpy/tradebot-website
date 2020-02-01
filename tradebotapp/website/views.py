0d976902cdde575d96fece13edfbadf348b85337
nealpatel <nealpatel2021@gmail.com>
2020-02-01 18:25:08 -0500

    hell(o) world :(

--- tradebotapp/website/views.py
+++ tradebotapp/website/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+#from .forms import NewTopicForm
+#from django.http import HttpResponse
+from .models import Trade, Position, Portfolio
 
 # Create your views here.
 def home(request):
-	print('Hello World!')
+	trades = []#Trade.objects.all()
+	return render(request, 'templates/home.html', {'trades': trades})
+	#print('Hello World!')
